cc93cee4831c5d8af476e488f5d855b2f8d4d0bf
tamanna141101 <tamannaislam.20100026@rpsu.edu.bd>
2022-07-09 07:06:51 +0600

    up

--- navbar.php
+++ navbar.php
@@ -31,14 +31,14 @@
                     <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active fw-bolder " style="color: #6610f2"
                                 aria-current="page" href="index.php">Home</a></li>
+                                <li class="nav-item"><a class="nav-link text-light " aria-current="page" href="about.php">About
+                                us</a></li>
+                                <li class="nav-item"><a class="nav-link text-light " aria-current="page" href="gallery.php">Gallery</a>
+                        </li>
                         <li class="nav-item"><a class="nav-link text-light   " aria-current="page"
                                 href="destination.php">Destination</a></li>
-                        <li class="nav-item"><a class="nav-link text-light  " aria-current="page"
-                                href="package.php">Package</a></li>
-                        <li class="nav-item"><a class="nav-link text-light " aria-current="page" href="gallery.php">Gallery</a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link text-light " aria-current="page" href="about.php">About
-                                us</a></li>
+                       
+                        
                         <li class="nav-item"><a class="nav-link text-light " aria-current="page" href="contact.php">Contact
                                 us</a></li>
                         <li class="nav-item"><a class="nav-link text-light " aria-current="page"
